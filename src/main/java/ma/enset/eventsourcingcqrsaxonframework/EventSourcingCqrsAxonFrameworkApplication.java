900585ceb4a015eba5d672964fac38c003147870
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-12-01 18:05:54 +0100

    Initial commit

---
+++ src/main/java/ma/enset/eventsourcingcqrsaxonframework/EventSourcingCqrsAxonFrameworkApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.eventsourcingcqrsaxonframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingCqrsAxonFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSourcingCqrsAxonFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/eventsourcingcqrsaxonframework/EventSourcingCqrsAxonFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.eventsourcingcqrsaxonframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingCqrsAxonFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
