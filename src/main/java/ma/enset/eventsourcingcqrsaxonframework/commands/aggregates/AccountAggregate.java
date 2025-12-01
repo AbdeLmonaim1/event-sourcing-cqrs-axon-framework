dfe5d13f7bc9f3e4d6e33055c4ba922d5a05de28
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-12-01 19:12:38 +0100

    handling event and updating the aggregate state

--- src/main/java/ma/enset/eventsourcingcqrsaxonframework/commands/aggregates/AccountAggregate.java
+++ src/main/java/ma/enset/eventsourcingcqrsaxonframework/commands/aggregates/AccountAggregate.java
@@ -2,7 +2,10 @@ package ma.enset.eventsourcingcqrsaxonframework.commands.aggregates;
 
 import ma.enset.eventsourcingcqrsaxonframework.commands.commands.AddAccountCommand;
 import ma.enset.eventsourcingcqrsaxonframework.enums.AccountStatus;
+import ma.enset.eventsourcingcqrsaxonframework.events.AccountCreatedEvent;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 
 @Aggregate
@@ -20,5 +23,18 @@ public class AccountAggregate {
         //here we verify the command data (business logic)
         if (command.getInitialBalance()<=0) throw new IllegalArgumentException("Initial balance must be greater than zero");
         //everything is ok! so now we can apply event (we must create event first)
+        AggregateLifecycle.apply(new AccountCreatedEvent(
+           command.getId(),
+           command.getInitialBalance(),
+                AccountStatus.CREATED,
+              command.getCurrency()
+        ));
+    }
+    //for handling event and updating the aggregate state
+    @EventSourcingHandler
+    public void on(AccountCreatedEvent event){
+        this.accountId= event.getAccountId();
+        this.balance= event.getInitialBalance();
+        this.status= event.getAccountStatus();
     }
 }
